8f2c2574b906caa09401b020c5336f671ade73e4
Kevin Kuebler <kevinkuebler@gmail.com>
2016-01-05 09:19:31 -0500

    Initial control code, from sample

--- bot.js
+++ bot.js
@@ -10,6 +10,69 @@ var board = new five.Board({
 
 board.on("ready", function() {
 
-  (new five.Led("D7")).strobe();
+  console.log('ready');
+
+  var rightWheel = new five.Motor({
+    pins: { pwm: "D0", dir: "D4" },
+    invertPWM: true
+  });
+
+  var leftWheel = new five.Motor({
+    pins: { pwm: "D1", dir: "D5" },
+    invertPWM: true
+  });
+
+  var speed = 255;
+
+  function reverse() {
+    leftWheel.rev(speed);
+    rightWheel.rev(speed);
+  }
+
+  function forward() {
+    leftWheel.fwd(speed);
+    rightWheel.fwd(speed);
+  }
+
+  function stop() {
+    leftWheel.stop();
+    rightWheel.stop();
+  }
+
+  function left() {
+    leftWheel.rev(speed);
+    rightWheel.fwd(speed);
+  }
+
+  function right() {
+    leftWheel.fwd(speed);
+    rightWheel.rev(speed);
+  }
+
+  function exit() {
+    leftWheel.rev(0);
+    rightWheel.rev(0);
+    setTimeout(process.exit, 1000);
+  }
+
+
+  var keyMap = {
+    'up': forward,
+    'down': reverse,
+    'left': left,
+    'right': right,
+    'space': stop,
+    'q': exit
+  };
+
+  var stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.resume();
+
+  stdin.on("keypress", function(chunk, key) {
+      if (!key || !keyMap[key.name]) return;      
+
+      keyMap[key.name]();
+  });
 
 });
